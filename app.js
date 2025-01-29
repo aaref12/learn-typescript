d07b64630c2a9fb038152bb1c5e50d34f1acc616
shaikh aaref hamza <skaarefz@gmail.com>
2025-01-29 11:40:51 +0530

    cor data types in ts

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-var a = 10;
-console.warn(a);
+// let a:number=10
+// console.warn(a)
+//cor data types in typescript
+//1) number 
+function add(a, b) {
+    return a + b;
+}
+var result = add(2.5, 5);
+console.warn(result);
+// string
+function printName(a) {
+    return a;
+}
+var Name = printName('coder');
+console.log(Name);
+// boolean
+function boolean(b) {
+    return b;
+}
+var value = boolean(true);
+console.warn(value);
--- app.ts
+++ app.ts
@@ -1,2 +1,29 @@
-let a:number=10
-console.warn(a)
+// let a:number=10
+// console.warn(a)
+
+
+//cor data types in typescript
+
+//1) number 
+function add(a:number,b:number){
+    return a+b
+}
+
+let result=add(2.5,5)
+console.warn(result)
+// string
+function printName(a:string){
+    return a
+}
+
+const Name=printName('coder')
+console.log(Name)
+
+// boolean
+
+function boolean(b:boolean){
+return b
+}
+const value=boolean(true)
+
+console.warn(value)
