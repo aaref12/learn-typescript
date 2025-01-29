ac3d443ea32d8799467149f3150a28d6807f715f
shaikh aaref hamza <skaarefz@gmail.com>
2025-01-29 12:24:58 +0530

    add object type

---
+++ object.js
@@ -0,0 +1,12 @@
+var user = {
+    name: 'aaref',
+    age: 23,
+    email: "shaikhaaref@gmail.com"
+};
+var userName = {
+    name: 'shaikh',
+    age: 24,
+    email: 'skaaref@gmail.com'
+};
+console.log(userName);
+console.log(user);
---
+++ object.ts
@@ -0,0 +1,17 @@
+// object type
+type objtype={name:string; age:number; email:string}
+
+const user:objtype={
+    name: 'aaref',
+    age:23,
+    email:"shaikhaaref@gmail.com"
+}
+
+const userName:objtype={
+    name:'shaikh',
+    age:24,
+    email:'skaaref@gmail.com'
+}
+console.log(userName)
+
+console.log(user)
